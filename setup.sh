771a0986906ea6221edc795ccb5dd9653beaa5a9
Alkio <46175458+alkiopprroo@users.noreply.github.com>
2022-01-16 16:53:34 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+
+mkdir ~/.app_def
+
+
+echo "[ + ] ( 1 ) - Cambiar Host"
+echo "[ + ] ( 2 ) - Cambiar IP"
+echo "[ + ] ( 3 ) - Cambiar resolv"
+echo "[ + ] ( 4 ) - Instalar apts"
+echo "[ + ] ( F ) - Fonts ( ROOT ) "
+echo "[ + ] ( 5 ) - Instalar zsh + zshrc + div " 
+#echo "[ + ] ( 6 ) - Alias" 
+#echo "[ + ] ( 7 ) - Instalar term div ejecutar desde la ruta" 
+
+read var_opcion
+
+if [ $var_opcion = 1 ]
+then
+    	echo "nuevo nombre del host: "
+    	read new_hostname
+    	read old_hostname < /etc/hostname
+    	sed -i "s/"$old_hostname"/"$new_hostname"/g" /etc/hostname
+    	sed -i "s/"$old_hostname"/"$new_hostname"/g" /etc/hosts
+    	hostname $new_hostname
+fi 
+
+if [ $var_opcion = 2 ]
+then
+	echo "nueva IP ( def = 192.168.1.X ): "
+	read new_address
+    	echo "nueva mascara (def = 255.255.255.0 ): "
+   	read new_netmask
+    	echo "nuevo gateway ( def = 192.168.1.1 ): "
+ 	read new_gateway
+    	echo "nueva interfaz (def = eth0 ): "
+    	read new_interface
+    	echo "
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback 
+
+auto "$new_interface"
+iface "$new_interface" inet static
+	address "$new_address"
+        netmask "$new_netmask"
+        gateway "$new_gateway > /etc/network/interfaces
+    ifdown $new_interface
+    ifup $new_interface
+    systemctl restart networking.service
+fi
+
+if [ $var_opcion = 3 ]
+then
+	# Resolv.conf
+	echo "Nuevo servidor DNS: ( def = 8.8.8.8 ) "
+	read new_dns
+	echo "nameserver $new_dns " > /etc/resolv.conf
+fi
+
+if [ $var_opcion = 4 ]
+then
+	# APTs
+	apt update -y
+	apt install neovim -y 
+	apt install vifm -y
+	apt install tmux -y
+	apt install curl -y
+	apt install git -y
+	apt install git-core -y 
+	apt install htop -y
+	apt install neofetch -y
+	apt install tree -y
+fi
+
+if [ $var_opcion = 'F' ]
+then
+	cd /usr/local/share/fonts
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+	unzip Hack.zip
+	rm Hack.zip
+fi
+
+if [ $var_opcion = 5 ]
+then
+	#ZSH
+	apt install zsh -y
+	
+	#COPYS
+	cp p10k.zsh ~/.p10k.zsh
+	cp zshrc ~/.zshrc
+	cp zellij ~/.app_def/zellij
+	cp config.yaml ~/.app_def/config.yaml
+	chmod a+x ~/.app_def/zellij
+
+	#PLUGIN
+	mmkdir ~/.app_def/zsh-plugins
+	cd ~/.zsh-plugins
+
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+	wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+	
+	cd ~ 
+	
+	# OHMYZSH + FONTS & STYLES 
+	apt install fonts-powerline -y
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -s
+	git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+#if [ $var_opcion = 6 ]
+#then
+	## ADDs 
+	#echo alias "z='zsh'" >> ~/.bashrc
+	#echo alias "ll='ls -l'" >> ~/.zshrc
+	#echo alias "lh='ls -lh' | more" >> ~/.zshrc
+	#echo alias "nv='nvim -u ~/.app_def/nvim.conf'" >> ~/.zshrc
+	#source ~/.zshrc
+	#echo " z = zsh // ll = ls -l // lh = ls -lh // v = nvim "
+#fi
+
+#if [ $var_opcion = 7 ]
+#then
+	#cp zellij ~/app_def/zellij
+	#cp config.yaml ~/app_def/config.yaml
+	#chmod a+x ~/app_def/zellij
+	#echo alias "div='~/app_def/zellij -c ~/app_def/config.yaml options --disable-mouse-mode'" >> ~/.zshrc
+	#zsh
+#fi
+
